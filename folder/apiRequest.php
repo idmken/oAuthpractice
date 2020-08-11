53a70631f88bf5bb8cb60d73613ba3dbce7ca030
Ken Myers <61115074+idmken@users.noreply.github.com>
2020-08-11 09:35:58 -0400

    Update apiRequest.php

--- folder/apiRequest.php
+++ folder/apiRequest.php
@@ -18,3 +18,60 @@ function apiRequest($url, $post=FALSE, $headers=array()) {
   $response = curl_exec($ch);
   return json_decode($response, true);
 }
+
+// Fill these out with the values from GitHub
+$githubClientID = '';
+$githubClientSecret = '';
+ 
+// This is the URL we'll send the user to first
+// to get their authorization
+$authorizeURL = 'https://github.com/login/oauth/authorize';
+ 
+// This is the endpoint we'll request an access token from
+$tokenURL = 'https://github.com/login/oauth/access_token';
+ 
+// This is the GitHub base URL for API requests
+$apiURLBase = 'https://api.github.com/';
+ 
+// The URL for this script, used as the redirect URL
+$baseURL = 'https://' . $_SERVER['SERVER_NAME']
+    . $_SERVER['PHP_SELF'];
+ 
+// Start a session so we have a place to
+// store things between redirects
+session_start();
+
+// If there is an access token in the session
+// the user is already logged in
+if(!isset($_GET['action'])) {
+  if(!empty($_SESSION['access_token'])) {
+    echo '<h3>Logged In</h3>';
+    echo '<p><a href="?action=repos">View Repos</a></p>';
+    echo '<p><a href="?action=logout">Log Out</a></p>';
+  } else {
+    echo '<h3>Not logged in</h3>';
+    echo '<p><a href="?action=login">Log In</a></p>';
+  }
+  die();
+}
+
+// Start the login process by sending the user
+// to GitHub's authorization page
+if(isset($_GET['action']) && $_GET['action'] == 'login') {
+  unset($_SESSION['access_token']);
+ 
+  // Generate a random hash and store in the session
+  $_SESSION['state'] = bin2hex(random_bytes(16));
+ 
+  $params = array(
+    'response_type' => 'code',
+    'client_id' => $githubClientID,
+    'redirect_uri' => $baseURL,
+    'scope' => 'user public_repo',
+    'state' => $_SESSION['state']
+  );
+ 
+  // Redirect the user to GitHub's authorization page
+  header('Location: '.$authorizeURL.'?'.http_build_query($params));
+  die();
+}
